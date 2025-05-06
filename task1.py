b495d5c6376f716ba55240d8b289cacc3c3b1ed7
Your Name <your.email@example.com>
2025-05-06 07:30:58 -0700

    chech the task1

---
+++ task1.py
@@ -0,0 +1,874 @@
+##                         ##print beheaviour(12/03/2025)
+##
+##
+##
+####1Variable
+##
+##x=10
+##y=20
+##print(x+y)
+##
+####2String
+##
+##a="apple"
+##b="banana"
+##print(a,b)
+##
+####3Escape sequence
+##
+##print("Mailam engineering college is a best college in villupuram\n")
+##print("Mailam enginnering college has won the\t overall\t championship\n in sports")
+##
+####4Multiline string
+##
+##a='''Mailam engineering college
+##is a best college
+##in villupuram'''
+##print(a)
+##
+####5Parameter
+##
+####,end=''
+##
+##print("bharani",end=" ")
+##print("velan")
+##
+####,sep=''
+##
+##print("bharani","velan","muthukumaran",sep="-")
+
+
+
+
+
+                                        ##task(13\03\2025)
+
+######1
+##
+##a="apple"
+##b="goa"
+##c="banana"
+##print(a,b,c,sep=",",end="!")
+##
+####2
+##
+##a=1
+##b=2
+##c=3
+##d=4
+##print(a,b,c,d)
+##
+##
+##a='''1 2 3 4'''
+##print (a,sep=",")
+##
+##
+##
+####3
+##
+##a='bharani'
+##b='velan'
+##c=21
+##print(a,b,c,sep=" | ")
+##
+####4
+##
+##a="A B C D"
+##b="E F G H"
+
+##                 ##task 18/03/2025
+##
+####question 1
+##
+##print("Name:bharani","age:21","country:india",sep="|")
+##
+####qestion2
+##
+##x=10
+##y=15.5
+##z="HELLO"
+##a=True
+##print(type(x))
+##print(type(y))
+##print(type(z))
+##print(type(a))
+##
+####question3
+##
+####add
+##a=10
+##b=20
+##
+####sub
+##A=50
+##B=15
+##
+####mul
+##x=2
+##y=4
+##
+####divide
+##X=100
+##Y=4
+##
+##print(a+b)
+##print(A-B)
+##print(x*y)
+##print(X/Y)
+##
+####question4
+##
+##num=50
+##
+##num+=10
+##
+##num-=10
+##
+##num*=2
+##
+##num/=5
+##
+##print(num)
+##
+####question5
+##
+##print('''Hello,world! \nWelcome to python \nHappy learning!''')
+##
+####question6
+##
+##a=10
+##b=5.5
+##print(type(a+b),a+b)
+##print(type(a*b),a*b)
+##print(type(a/b),a/b)
+##
+##
+####question7
+##
+##a=5
+##print(a,"X 1=",1*a)
+##print(a,"X 2=",2*a)
+##print(a,"X 3=",3*a)
+##print(a,"X 4=",4*a)
+##print(a,"X 5=",5*a)
+
+
+                                  ##task on 19/03/2025
+
+
+
+######question1
+##
+##a=15
+##b=4
+##print("add:",a+b,"| sub:",a-b,"| mul:",a*b,"| divide:",a/b,"| modulus:",a%b,end="\n\n")
+##
+####question2
+##
+##first_name=input("first_name:")
+##last_name=input("last_name:")
+##age=input("age:")
+##print(first_name,last_name,age,sep="|",end="\n-----\n")
+##
+##
+####question3
+##
+##a="python"
+##b="rocks"
+##print(a+" "+b,sep=" ---> ",end="!!")
+##
+####question4
+##
+##a=float(input("first_number:"))
+##b=float(input("second_number:"))
+##print("Addition:",a + b, "Subtraction:")
+####
+######question5
+####
+####a=[1,2,3,4,5]
+####print(a,sep="-",end="!!")
+##
+##
+##x=10
+##y=15
+##x,y=y,x
+##print(x,y)
+
+                   ## 24/03/2025
+
+##a=int(input())
+##
+##if(a<0):
+##    print("a is negative")
+##elif(a>0):
+##    print("a is positive")
+##else:
+##    print("zero")
+##
+####get two input find which is greater
+##
+##a=int(input("Enter a Number:"))
+##b=int(input("Enter a Number:"))
+##
+##if a>b:
+##    print("a is greater")
+##elif a<b:
+##    print("b is greater")
+##else:
+##    print("nothing")
+
+####get three input find which is greater
+##
+##a=int(input("Enter a Number:"))
+##b=int(input("Enter a Number:"))
+##c=int(input("Enter a Number:"))
+##if a>b and a>c:
+##    print("a is greater")
+##elif b>a and b>c:
+##    print("b is greater")
+##elif c>a and c>b:
+##    print("c is greater")
+##else:
+##    print("given integer is true")
+
+######get a input find odd r even
+##
+##a=int(input("Enter a number:"))
+##
+##if (a%2==0):
+##    print("a is even")
+##elif(a%2!=0):
+##    print("a is odd")
+##else:
+##    print("output is invaild")
+##
+##
+######get a input and find the grade of the students
+
+##a=int(input("Enter a student's mark:"))
+##
+##if a>=90:
+##    print("grade:A")
+##elif a>=80:
+##     print("grade:B")
+##elif a>=70:
+##     print("grade:C")
+##elif a>=60:
+##     print("grade:D")
+##elif a>=50:
+##     print("grade:c")
+##else:
+##     print("student is failed in the exam")
+
+                                        ##25/03/2025
+##divisible 3 r not
+##
+##a=int(input("Enter a number:"))
+##
+##if a%3==0:
+##    print("a is divisible by 3")
+##elif a%3!=0:
+##    print("a is not divisible by 3")
+##else:
+##    print("the value is not divisible")
+##
+####age difference
+##
+##a=int(input("Enter a number:"))
+##if a>=18:
+##    print("old enough")
+##else:
+##    print("too young")
+##
+####question 4
+##
+##a=str(input("Enter your name:"))
+##b=str(input("Enter your friends name:"))
+##
+##if a==b:
+##    print("hi..your are always welcome")
+##elif a!=b:
+##    print(" ")
+##else:
+##    print("nothing")
+##
+####leap year r not
+##
+##a=int(input("Enter a year:"))
+##
+##if(a%4==0 and a%100!=0):
+##    print("leap year")
+##    
+##else:
+##    print("not a leap year")
+##
+####question6
+##
+##a=int(input("Enter a number:"))
+##b=int(input("Enter a number:"))
+##
+##if a>b:
+##    print("a is larger")
+##elif a==b:
+##    print("both are equal")
+##else:
+##    print("b is larger")
+##
+####vowels
+##
+##a=str(input("Enter a character:"))
+##if a in 'character':
+##    print("a is vowel")
+##else:
+##    print("a is not a vowel")
+##
+####age difference using a variable
+##a=int(input("Enter a age:"))
+##if a>=18:
+##    print("old enough to vote")
+##else:
+##    print("too young for voting")
+##
+####question 10
+##
+##a=str(input("Enter your password:"))
+##b=str(input("Enter your password:"))
+##if a not in b:
+##    print("Enter your password")
+##elif  a in b:
+##    print("correct! both the passwords are same")
+##else:
+##    print("incorrect password")
+
+
+
+                                            ##26/03/2025
+
+## sum of natural number in for loop
+
+##number=int(input("enter a number:"))
+##
+##for i in range(number):
+##    print(i)
+##get the input and sum with user input
+##
+##number=int(input("Enter a Nmber:"))
+##result=0
+##for i in range(number):
+##    result+=i
+##print(result)
+##    
+####odd number using for loop
+##
+##odd_num=int(input("Enter a number:"))
+##for i in range(1,odd_num+2):
+##     if i%3==0:
+##         print(i)
+####
+####even number using for loop
+##
+##even_num=int(input("Enter a number:"))
+##for i in range(1,even_num+1):
+##    if i%2==0:
+##        print(i)
+##
+
+####/5
+##
+##a=int(input("Enter a number:"))
+##for i in range(0,a+1,5):
+##    print(i)
+                                         ##27/03/2025
+
+####pattern
+##pattern=int(input("print the pattern:"))
+##for i in range(pattern+5):
+##     print("*"*i)
+
+
+####factorial
+##
+##factorial=int(input("print a factorial number:"))
+##n=1
+##for i in range(1,factorial+1):
+##    n*=i
+##print(n)
+
+##table
+##
+##
+##table=int(input("Enter a number"))
+##for i in range(1,table):
+##    print(i,"X",i,"=",table*i)
+##    
+
+                                             ##01/04/2025
+
+##add the number using for loop
+##
+##num=input("Enter a input:")
+##a=0
+##for i in num:
+##    a+=int(i)
+##    print(a)
+
+
+##reverse the number using for loop
+
+##a=input("Reverse the number:")
+##b=0
+##for i in range(len(a)):
+##    temp=int(a)%10
+##    a=int(a)//10
+##    b=(b*10)+temp
+##    print(b)
+
+####fibonacci series
+
+##n=int(input("Enter the number of terms:"))
+##a=0
+##b=1
+##for i in range(n):
+##    a,b=b,a+b 
+##    print(a)
+##
+####armstrong number
+##
+##n=input("Enter a number:")
+##a=0
+##for i in n:
+##    a=a+int(i)**len(n)
+##    print(a)
+
+                            ##4/4/2025
+####check the given user input as odd r even
+##
+##num=int(input("print odd or even:"))
+##
+##for i in range(1,num):
+##    if i&1==1:
+##        print("odd")
+##    else:
+##        print("even")
+##         
+##num=int(input("print odd or even:"))
+##
+##for i in range(1,num):
+##    if i&1==1:
+##        print(i,"is odd")
+##    else:
+##        print(i,"is even")
+
+
+##find the missing number using for loop
+
+##num=int(input("Enter a number:"))
+##a=0
+##
+##for i in range(num-1):
+##  
+##    a = a + num
+##    b=num*(num+1)//2
+##    missing = b - a
+##    print("missing number",missing)
+
+
+
+                          ##14/04/2025
+
+####odd r even
+##
+##n=int(input("Enter a number:"))
+##if n%2==0:
+##    print("even")
+##else:
+##    print("odd")
+##
+####largest of three numbers
+##a=int(input("Enter a number:"))
+##b=int(input("Enter a number:"))
+##c=int(input("Enter a number:"))
+##if a>=b and a>=c:
+##    print("a is largest number")
+##elif b>=a and b>=c :
+##    print("b is largest number")
+##else:
+##    print("c is largest number")
+##
+####smallest of three numbers
+##a=int(input("Enter a number:"))
+##b=int(input("Enter a number:"))
+##c=int(input("Enter a number:"))
+##
+##if a<=b and a<=c:
+##    print("a is smallest number")
+##elif b<=a and b<=c :
+##    print("b is smallest number")
+##else:
+##    print("c is smallest number")
+##
+####number positive,negative r zero
+##
+##n= int(input("Enter a number: "))
+##
+##if n > 0:
+##    print("Positive number")
+##elif n < 0:
+##    print("Negative number")
+##else:
+##    print("Zero")
+##
+##
+####leap year or not
+##
+##a=int(input("Enter a year:"))
+##
+##if(a%4==0 and a%100!=0):
+##    print("leap year")
+##    
+##else:
+##    print("not a leap year")
+##
+####fibonacci series
+##
+##n=int(input("Enter the number :"))
+##a=0
+##b=1
+##for i in range(n):
+##    a,b=b,a+b 
+##    print(a)
+##    
+####reverse the number using for loop
+##
+##a=input("Reverse the number:")
+##b=0
+##for i in range(len(a)):
+##    c=int(a)%10
+##    a=int(a)//10
+##    b=(b*10)+c
+##    print(b)
+##
+####palidrome
+##
+##num = input("Enter a number: ")
+##
+##if num == num[::-1]:
+##    print("the number is palindrome")
+##else:
+##    print("the number is not a palindrome")
+##
+#### multipilication table
+##a=int(input("Enter a number:"))
+##for i in range(1,11):
+##    print(i,"x 2 =",i*2)
+##
+####print all even numbers between 1 to 50
+##n=int(input("Enter a number:"))
+##for i in range(0, 51):
+##    if i % 2 == 0:
+##        print(i)
+##
+####print all odd numbers between 1 to 50
+##n=int(input("Enter a number:"))
+##for i in range(1, 51):
+##    if i % 2!= 0:
+##        print(i)
+##   
+##
+####armstrong number
+##
+##n=input("Enter a number:")
+##a=0
+##for i in n:
+##    a=a+int(i)**len(n)
+##    print(a)
+    
+####swap the number
+##a = int(input("Enter a number:"))
+##b = int(input("Enter a number:"))
+##a, b = b, a
+##print(a,b)
+##
+####count the digits in a number
+##n = int(input("Enter a number: "))
+##print(len(str(n)))
+##
+
+####print all numbers divisible by 7 between 1 to 100
+##a=int (input("Enter a number"))
+##for i in range(1, 101):
+##    if i % 7 == 0:
+##        print(i)
+
+
+
+##                                    21/04/2025
+##
+####create a set of 5 fruits
+##
+##fruits={"apple","banana","cherry","mango","rassberry"}
+##
+##print(fruits)
+##
+####add a new fruit in set
+##
+##fruits={"apple","banana","cherry"}
+##fruits.add("mango")
+##print(fruits)
+##
+##
+####remove an item from the set using remove and discard
+##
+##
+##fruits={"apple","banana","cherry"}
+##fruits.remove("banana")
+##print(fruits)
+##
+##
+##fruits={"apple","banana","cherry"}
+##fruits.discard("banana")
+##print(fruits)
+##
+##
+##
+####check if apple in a set
+##
+##fruits={"apple","banana","cherry"}
+##if "apple" in fruits:
+##    print("true")
+##
+####create a set with duplicate items
+##
+##set1={"apple","banana","cherry","apple"}
+##
+##print(set1)
+##
+##
+####find the union
+##
+##a={1,2,3}
+##b={3,4,5}
+##c=a|b
+##print(c)
+##
+##
+####intersection
+##a={1,2,3}
+##b={3,4,5}
+##c=a&b
+##print(c)
+##
+##
+####difference
+##
+##a={1,2,3}
+##b={3,4,5}
+##c=a-b
+##print(c)
+##
+####subset
+##a={"a","b","c"}
+##b={"f","g","c","b","a"}
+##c=a.issubset(b)
+##print(c)
+##
+##
+####clear all items in sets
+##
+##a={"apple","banana","cherry"}
+##a.clear()
+##print(a)
+##
+##
+####write a program to remove the duplicate from list
+##
+##a=set(("apple","banana","cherry","apple"))
+##print(a)
+##
+##
+####two lists find the common elements using sets
+##
+##a=set((1,2,3))
+##b=set((3,4,5))
+##c=a&b
+##print(c)
+##
+##
+####find the number of unique character in set
+##
+##a={"a","b","c"}
+##b={"f","g","s"}
+##c=a-b
+##print(c)
+##
+####check the two sets are disjoint
+##
+##a={"bharani","velan"}
+##b={"ryan","micheal"}
+##c=a.isdisjoint(b)
+##print(c)
+##
+####give a student name and print only the unique name
+##
+##name_list1=set(("bharani","velan","muthu"))
+##name_list2=set(("dharani","ryan","micheal"))
+##name_list3=name_list1-name_list2
+##print(name_list3)
+##
+
+                                    ##22/04/2025
+
+####create a list of 10 numbers
+##
+##num=[1,2,3,4,5,6,7,8,9]
+##print(num)
+
+####access and print the first and last element of the list
+##
+##a=["apple","banana","cherry"]
+##print(a[0])
+##print (a[1])
+
+
+####append a new elements to the list
+##
+##a=["apple","banana","cherry"]
+##a.append("blueberry")
+##print(a)
+
+
+####insert a element at the second position
+##
+##a=["apple","banana","cherry"]
+##a.insert(2,"blueberry")
+##print(a)
+
+
+##remove the element by value and index
+##
+##a=["apple","banana","cherry","blueberry"]
+##a.remove("cherry")
+##print(a)
+
+##a=["apple","banana","cherry","blueberry"]
+##a.pop(2)
+##print(a)
+
+####reverse the without using slicing
+##
+##a=[1,2,3,4,5,6,7,8,9,10]
+##a.reverse()
+##print(a)
+
+
+
+
+##                                 ##23/04/2025
+######add keyvalue pairs
+####
+##a={}
+##a.update(Name="alice",Age="25",City="new york")
+##print(a)
+####
+##a["Email"]="alice@gmail.com"
+##a["Phone"]=123456789
+##print(a)
+####
+##a["age"]=26
+##a["city"]="india"
+##print(a)
+##
+####remove the specific key
+##del a["Name"]
+##print(a)
+##
+####pop to remove email
+##a.pop("Email")
+##print(a)
+##a["removed_email"]="alice@gmail.com"
+##print(a)
+
+                                                      ##26/04/2025
+####create a dictionaries with keys as fruits name and values as their prices
+##
+##fruits={"apple": "$200","banana":"$80","cherry":"$300"}
+##print(fruits)
+
+####access the value of key in dictionary and print it
+##
+##fruits={"apple": "$200","banana":"$80","cherry":"$300"}
+##print(fruits["banana"])
+
+####update the price of fruits in the dictionary
+##fruits={"apple": "$200","banana":"$80","cherry":"$300"}
+##fruits["cherry"]= "$400"
+##print(fruits)
+
+####add a new fruit to the dictionary with its prices
+##fruits={"apple": "$200","banana":"$80","cherry":"$300"}
+##fruits["blueberry"]= "$400"
+##print(fruits)
+
+####remove the key value pairs from the dictionary
+##
+##fruits={"apple": "$200","banana":"$80","cherry":"$300"}
+##fruits.pop("apple")
+##print(fruits)
+
+####check if a key excits in the in operators
+##fruits={"apple": "$200","banana":"$80","cherry":"$300"}
+##if "cherry" in fruits:
+##    print(fruits)
+
+####loop through all keys and print them
+##
+##fruits={"apple": "$200","banana":"$80","cherry":"$300"}
+##for i in fruits.keys():
+##    print(i)
+
+####loop all values and print them
+##fruits={"apple": "$200","banana":"$80","cherry":"$300"}
+##for i in fruits.values():
+##    print(i)
+
+####loop all the key value pairs and print them
+##
+##fruits={"apple": "$200","banana":"$80","cherry":"$300"}
+##for i in fruits.items():
+##    print(i)
+
+####get the length of a dictionary using len
+##
+##fruits={"apple": "$200","banana":"$80","cherry":"$300"}
+##print(len(fruits))
+
+####create a nested dic for student name,age,sub with their marks
+##
+##student_record={"student 1":{"name":"Arjun","age":21,"sub":"python","mark":85},
+##                "student 2":{"name":"Gokul","age":21,"sub":"python","mark":80},
+##                "student 3":{"name":"ganesh","age":21,"sub":"python","mark":59}}
+##print(student_record)
+
+####merge two dictionaries using the | operator or update
+##
+##dic1={"a":1,"b":2}
+##dic2={"c":3,"d":4}
+##dic1.update(dic2)
+##print(dic1)
+
+####use get method to safely retrive a value and print a custom message if key is not found
+##
+##marks={"arun":55,"arjun":67,"gokul":30}
+##x=marks.get("ganesh","ganesh mark not found")
+##print(marks)
+
+values={x:x**2 for x in range(1,11)}
+print(values)
+
+students={"arun":90,"varun":97,"tarun":100}
+for i in students.values():
+     print(i)
+  
+     print(max[i])
+    
+
